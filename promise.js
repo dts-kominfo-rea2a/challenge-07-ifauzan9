8b0509ff52e4d2fe0f5a56d831cd6982ec65af6c
ifauzan <ifauzan9@gmail.com>
2022-06-09 22:14:25 +0700

    pengerjaan pertama promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async function (emosi) {
+  try {
+    let coba1 = await promiseTheaterIXX();
+    let coba2 = await promiseTheaterVGC();
+
+    let result = [...coba1, ...coba2];
+    let jumlahEmosi = result.filter((el, index) => {
+      if (el.hasil === emosi) {
+        return true;
+      }
+    });
+
+    return jumlahEmosi.length;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
